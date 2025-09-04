7c4f20b551ab0ebb69a333893daa76d7bca79952
estrella272729 <estrella272729@gmail.com>
2025-09-04 10:19:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("APP")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones")
 st.write("Para realizar cosas")
-image = Image.open('gatito.jpg')
+image = Image.open('gatito.jpeg')
 
 st.image(image, caption='Interfaces multimodales')
 
