00fbc546c8c652aadcfc11ea78fce33c9a1e080e
estrella272729 <estrella272729@gmail.com>
2025-10-21 23:59:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 st.title("Intro")
 
-st.header("Esta es mi primera aplicación")
+st.header("Esta es mi primera aplicación- Opinión de arte")
 st.write("Pintura de la noche estrellada")
 image = Image.open('Pintura.jpg')
 
 st.image(image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe tu opinión sobre la pintura:', 'Escribe aqui') 
-st.write('¡Gracias por tu opinión!', texto)
+st.write('¡Gracias por tu opinión!')
